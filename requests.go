d5ca05e0532011283394d7456aeeedefc048e501
takurx <taku3wzr@gmail.com>
2019-01-06 11:59:53 +0900

    methodのdebug文を追加
    methodのdebug文を追加

--- requests.go
+++ requests.go
@@ -39,7 +39,7 @@ func (c *Client) req(method, path string, body io.Reader, intercept func(*http.R
 	//r.Header.Add("Content-Length", len(string(bb)))
 	//r.Header.Add("Expect", "100-continue")
 
-	//log.Println("func req Method:", r.Method)
+	log.Println("func req Method:", r.Method)
 	//dumpReq, _ := httputil.DumpRequest(r, true)
 	//log.Println("func req DumpRequest:", string(dumpReq))
 
